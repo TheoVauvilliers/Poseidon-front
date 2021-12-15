e0a2118a1f3ce82f9e200029683124171046cc9d
Théo Vauvilliers <theo.vauvilliers10@gmail.com>
2021-12-15 12:11:02 +0100

    feat: Create first page, component and make api call on our local API

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header>
+      <div class="form">
+        <input v-model="name" placeholder="Write a username here" /> <br />
+        <button @click="handleClick()">Search</button>
+      </div>
+    </header>
+    <LiveWatchedsList :liveWatcheds="liveWatched" v-if="canRender"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import LiveWatchedsList from './components/LiveWatchedsList.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { LiveWatchedsList },
+  data() {
+    return {
+      name: null,
+      liveWatched: null,
+      canRender: false
+    }
+  },
+  methods: {
+    handleClick() {
+      axios
+        .get(`http://127.0.0.1:3001/user/${this.name}`)
+        .then((res) => {
+          const data = res.data.data
+
+          if (data) {
+            this.liveWatched = data.liveWatched
+            this.canRender = true
+          }
+        })
+        .catch((err) => {
+          console.log('failed', err)
+        })
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  header {
+    text-align: center;
+  }
+  header .form {
+    margin-top: 40px;
+  }
+  input {
+    margin: 10px;
+    padding: 8px 16px;
+    color: white;
+    border: 3px solid #1A1A1A;
+    background-color: #1A1A1A;
+    border-radius: 0;
+    font-weight: bold;
+  }
+  button {
+    margin: 10px;
+    color: white;
+    border: 3px solid #1A1A1A;
+    background-color: #1A1A1A;
+    padding: 8px 16px;
+    border-radius: 0;
+    cursor: pointer;
+    font-weight: bold;
+  }
 </style>
---
+++ src/components/LiveWatchedsList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="liveWatched-list">
+    <ul>
+      <li v-for="(liveWatched, index) in liveWatcheds" :key="index">
+        {{ index }}. Watched <span>{{ liveWatched.name }}</span> at <span>{{ liveWatched.watchedAt }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+      liveWatcheds: {
+          type: Array,
+          required: true
+      }
+  }
+});
+</script>
+
+<style scoped>
+  .liveWatched-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .liveWatched-list ul {
+    padding: 0;
+  }
+  .liveWatched-list li {
+    list-style-type: none;
+    padding: 16px;
+    margin: 16px 0;
+    background-color: #1A1A1A;
+  }
+  .liveWatched-list span {
+    font-style: italic;
+    color: wheat;
+  }
+</style>
